897eb50ffd2211082f240596234a52ffedf4e863
Admin <b>
2019-04-26 19:00:45 +0900

    update button

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class SpaceBox extends SizedBox {
+  SpaceBox({double width = 8.0, double height = 8.0})
+      : super(width: width, height: height);
+
+  SpaceBox.width([double value = 8.0]) : super(width: value);
+  SpaceBox.height([double value = 8.0]) : super(height: value);
+}
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
-
   final String noneTime = '--:--';
+  final saturdayColor = Colors.blueAccent.shade100;
+  final sundayColor = Colors.redAccent.shade100;
+  final normalColor = Colors.white;
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
@@ -30,6 +40,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
 
   TabController _tabController;
+
+  int _normalWorkingHours = 0;
+  int _actualWorkingHours = 0;
 
   List<String> stTimes = <String>[];
   List<String> edTimes = <String>[];
@@ -40,9 +53,13 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
     for(var i = 4; i <= 4; i++) {
       tabs.add(Container(
         width: mediaSize.width,
-        height: 20.0,
         alignment: Alignment.center,
-        child: new Text(i.toString() + '月'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+          new Text(i.toString() + '月'),
+          new Text('稼働時間 : ( $_actualWorkingHours/$_normalWorkingHours )h'),
+        ],),
       ));
     }
     return tabs;
@@ -72,20 +89,35 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
     }
   }
 
+  Color _getDayColor(int month, int day){
+    var week = new DateTime(2019, month + 1, day).weekday;
+    if(week == DateTime.saturday){
+      return widget.saturdayColor;
+    } else if(week == DateTime.sunday){
+      return widget.sundayColor;
+    } else {
+      return widget.normalColor;
+    }
+  }
+
   List<Widget> _getDayList(var month){
     List<Widget> days = <Widget>[];
     final lastDayOfMonth = new DateTime(2019, month + 1, 0);
-
     days.addAll(List.generate(lastDayOfMonth.day, (i) => i).map((int i){
       stTimes.add(widget.noneTime);
       edTimes.add(widget.noneTime);
       return Container(
-        padding: const EdgeInsets.all(32.0),
+        padding: const EdgeInsets.all(16.0),
+        decoration: new BoxDecoration(
+            color: _getDayColor(month, i),
+            border: new Border(bottom: new BorderSide(color: Colors.black12, width: 1.0))
+        ),
         child: Row(
           children: <Widget>[
             Expanded(child: Column(
               children: [
                 Container(
+                  alignment: Alignment.topLeft,
                   child: Text((i+1).toString() + '日'),
                 ),
                 Container(
@@ -101,9 +133,9 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
                             stTimes[i] = selectTime.format(context);
                           });
                         },
-                        child: new Text(stTimes[i]),
+                        child: new Text(stTimes[i], style: TextStyle(fontSize: 20.0),),
                       ),
-                      Text(' ～ '),
+                      Text('～', style: TextStyle(fontSize: 20.0),),
                       new FlatButton(
                         onPressed: () async {
                           TimeOfDay selectTime = await showTimePicker(
@@ -114,11 +146,51 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
                             edTimes[i] = selectTime.format(context);
                           });
                         },
-                        child: new Text(edTimes[i]),
+                        child: new Text(edTimes[i], style: TextStyle(fontSize: 20.0),),
                       ),
                     ],
                   ),
-                )
+                ),
+               Row(
+                children: <Widget>[
+                  new MaterialButton(
+                    minWidth: 30.0,
+                    textColor: Colors.white,
+                    color: Colors.blue,
+                    onPressed: (){
+                      setState(() {
+                        stTimes[i] = TimeOfDay(hour: 9, minute: 0).format(context);
+                        edTimes[i] = TimeOfDay(hour: 18, minute: 0).format(context);
+                      });
+                    },
+                    child: new Text("定時"),
+                  ),
+                  SpaceBox.width(16.0),
+                  new MaterialButton(
+                    minWidth: 30.0,
+                    textColor: Colors.white,
+                    color: Colors.blue,
+                    onPressed: (){
+                      setState(() {
+                      });
+                    },
+                    child: new Text("休暇"),
+                  ),
+                  SpaceBox.width(16.0),
+                  new MaterialButton(
+                    minWidth: 30.0,
+                    textColor: Colors.white,
+                    color: Colors.blue,
+                    onPressed: (){
+                      setState(() {
+                        stTimes[i] = widget.noneTime;
+                        edTimes[i] = widget.noneTime;
+                      });
+                    },
+                    child: new Text("クリア"),
+                  ),
+                ],
+              ) ,
               ],
             ))
           ],
