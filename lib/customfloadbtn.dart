2e7b6f84a2882a6d3d3b1a031070836218d5f64d
Admin <b>
2019-06-11 20:50:38 +0900

    change float button

--- lib/customfloadbtn.dart
+++ lib/customfloadbtn.dart
@@ -4,126 +4,109 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class FloatBtnArea extends StatefulWidget {
   FloatBtnArea({Key key}) : super(key: key);
 
-  final IconData closeIcon = const IconData(59528, fontFamily: 'MaterialIcons');
-  final IconData openIcon = const IconData(58298, fontFamily: 'MaterialIcons');
-
   @override
   State<StatefulWidget> createState() => new FloatBtnAreaState();
 }
 
 class FloatBtnAreaState extends State<FloatBtnArea>
     with SingleTickerProviderStateMixin {
-  // main floatボタンの押下状態
-  bool floatOff = true;
-  double _opacity = 0.0;
-
-  IconData mainIcon;
 
   AnimationController animationController;
-
-  List<Widget> getFloatBtn() {
-    List<Widget> buttons = <Widget>[];
-
-    buttons.add(new Padding(
-      padding: EdgeInsets.all(8.0),
-      child: new AnimatedOpacity(
-        opacity: _opacity,
-        duration: const Duration(milliseconds: 500),
-        child: new FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.blueAccent,
-          child: Icon(const IconData(58051, fontFamily: 'MaterialIcons')),
-        ),
-      ),
-    ));
-    buttons.add(new Padding(
-      padding: EdgeInsets.all(8.0),
-      child: new AnimatedOpacity(
-        opacity: _opacity,
-        duration: const Duration(milliseconds: 500),
-        child: new FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.blueAccent,
-          child: Icon(const IconData(59506, fontFamily: 'MaterialIcons')),
-        ),
-      ),
-    ));
-    buttons.add(new Padding(
-      padding: EdgeInsets.all(8.0),
-      child: new FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            if (floatOff) {
-              floatOff = false;
-              _opacity = 1.0;
-              mainIcon = widget.openIcon;
-            } else {
-              floatOff = true;
-              _opacity = 0.0;
-              mainIcon = widget.closeIcon;
-            }
-          });
-        },
-        backgroundColor: Colors.pinkAccent,
-        child: new Icon(mainIcon),
-      ),
-    ));
-
-    return buttons;
-  }
 
   @override
   void initState() {
     super.initState();
-    mainIcon = widget.openIcon;
     animationController =
-        AnimationController(duration: Duration(milliseconds: 900), vsync: this);
+        AnimationController(duration: Duration(milliseconds: 400), vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: getFloatBtn(),
+    return new SectorAnimation(
+      controller: animationController,
     );
   }
 }
 
 class SectorAnimation extends StatelessWidget {
   SectorAnimation({Key key, this.controller})
-      : scale = Tween<double>(
-          begin: 1.5,
+      : _openScale = Tween<double>(
+          begin: 1.0,
           end: 0.0,
         ).animate(
-          CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn),
+          new CurvedAnimation(parent: controller, curve: Curves.linear),
+        ),
+        _closeScale = Tween<double>(
+          begin: 0.0,
+          end: 1.0,
+        ).animate(
+          new CurvedAnimation(parent: controller, curve: Curves.linear),
+        ),
+        _transTween1 =
+            Tween<Offset>(begin: Offset.zero, end: const Offset(0.0, -1.2))
+                .animate(
+          new CurvedAnimation(parent: controller, curve: Curves.linear),
+        ),
+        _transTween2 =
+        Tween<Offset>(begin: Offset.zero, end: const Offset(0.0, -2.4))
+            .animate(
+          new CurvedAnimation(parent: controller, curve: Curves.linear),
+        ),
+        _transTween3 =
+        Tween<Offset>(begin: Offset.zero, end: const Offset(0.0, -3.6))
+            .animate(
+          new CurvedAnimation(parent: controller, curve: Curves.linear),
         ),
         super(key: key);
+
   final AnimationController controller;
-  final Animation<double> scale;
+  final Animation<double> _openScale;
+  final Animation<double> _closeScale;
+  final Animation<Offset> _transTween1;
+  final Animation<Offset> _transTween2;
+  final Animation<Offset> _transTween3;
 
   build(context) {
-    return AnimatedBuilder(
+    return new AnimatedBuilder(
       animation: controller,
       builder: (context, builder) {
-        return Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Transform(
-                transform: new Matrix4.identity()
-                  ..scale(0.0, scale.value - 1.5),
-                child: FloatingActionButton(
-                    child: Icon(FontAwesomeIcons.timesCircle),
-                    onPressed: _close,
-                    backgroundColor: Colors.red)),
-            Transform(
-                transform: new Matrix4.identity()
-                  ..scale(0.0, scale.value),
-                child: FloatingActionButton(
-                    child: Icon(FontAwesomeIcons.timesCircle),
-                    onPressed: _open,
-                    backgroundColor: Colors.red)),
-          ],
-        );
+        return Stack(alignment: Alignment.center, children: <Widget>[
+          new SlideTransition(
+            position: _transTween3,
+            child: new FloatingActionButton(
+                child: new Icon(FontAwesomeIcons.playstation),
+                onPressed: null,
+                backgroundColor: Colors.red),
+          ),
+          new SlideTransition(
+            position: _transTween2,
+            child: new FloatingActionButton(
+                child: new Icon(FontAwesomeIcons.trashAlt),
+                onPressed: null,
+                backgroundColor: Colors.red),
+          ),
+          new SlideTransition(
+            position: _transTween1,
+            child: new FloatingActionButton(
+                child: new Icon(FontAwesomeIcons.cloudUploadAlt),
+                onPressed: null,
+                backgroundColor: Colors.red),
+          ),
+          new ScaleTransition(
+            scale: _openScale,
+            child: new FloatingActionButton(
+                child: new Icon(FontAwesomeIcons.plusCircle),
+                onPressed: _open,
+                backgroundColor: Colors.red),
+          ),
+          new ScaleTransition(
+            scale: _closeScale,
+            child: new FloatingActionButton(
+                child: new Icon(FontAwesomeIcons.timesCircle),
+                onPressed: _close,
+                backgroundColor: Colors.red),
+          ),
+        ]);
       },
     );
   }
