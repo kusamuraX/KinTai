b1f689d88de3565fb8f448f9de0e384d7ab2a6ba
Admin <b>
2019-07-12 22:06:13 +0900

    sheet v4

--- lib/sheet.dart
+++ lib/sheet.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:teskin/sheetObj/sheetJsonObj.dart';
 import 'package:googleapis/sheets/v4.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
@@ -22,7 +21,9 @@ signIn() async{
     final _client = new AuthHttpClient(headers);
 
     var sheetApi = new SheetsApi(_client);
-//
+
+    UpdateSpreadsheetPropertiesRequest();
+
 //    var getValue = await sheetApi.spreadsheets.values.get(spreadsheetId, 'sheet1!B2');
 //
 //    getValue.values.forEach((list) => {
@@ -31,23 +32,65 @@ signIn() async{
 //      })
 //    });
 
-    UpdateSpreadsheetProperties updateSpreadsheetProperties = new UpdateSpreadsheetProperties();
-    updateSpreadsheetProperties.setTitle('sheetEX');
+//    UpdateSpreadsheetProperties updateSpreadsheetProperties = new UpdateSpreadsheetProperties();
+//    updateSpreadsheetProperties.setTitle('sheetEX');
+//
+//    BatchRequest request = new BatchRequest();
+//    request.setRequestProperties(updateSpreadsheetProperties);
 
-    BatchRequest request = new BatchRequest();
-    request.setRequestProperties(updateSpreadsheetProperties);
 
-    BatchUpdateValuesRequest batchUpdateValuesRequest = new BatchUpdateValuesRequest();
-    var range = ValueRange.fromJson(request.toJsonMap());
 
-    sheetApi.spreadsheets.values.batchUpdate(batchUpdateValuesRequest, spreadsheetId);
+//    BatchUpdateValuesRequest batchUpdateValuesRequest = new BatchUpdateValuesRequest();
+//    var range = ValueRange.fromJson(request.toJsonMap());
+//
 
-    UpdateSheetPropertiesRequest updateSheetReq = UpdateSheetPropertiesRequest();
-    updateSheetReq.properties.title = '';
+    BatchUpdateSpreadsheetRequest batchUpdateSpreadsheetRequest = new BatchUpdateSpreadsheetRequest();
+    batchUpdateSpreadsheetRequest.requests = new List();
+
+//    // ファイル名
+//    var fileNameReq = new Request();
+//    fileNameReq.updateSpreadsheetProperties = new UpdateSpreadsheetPropertiesRequest();
+//    fileNameReq.updateSpreadsheetProperties.properties = new SpreadsheetProperties();
+//    fileNameReq.updateSpreadsheetProperties.properties.title = 'changeTest';
+//    fileNameReq.updateSpreadsheetProperties.fields = '*';
+//    batchUpdateSpreadsheetRequest.requests.add(fileNameReq);
+
+    // シート追加
+//    var sheetAddReq = new Request();
+//    AddSheetRequest _addSheetRequest = new AddSheetRequest();
+//    _addSheetRequest.properties = new SheetProperties();
+//    _addSheetRequest.properties.title = 'newSheet';
+//    sheetAddReq.addSheet = _addSheetRequest;
+//    batchUpdateSpreadsheetRequest.requests.add(sheetAddReq);
+
+    // シート内容のクリア
+    var sheetContentsClearReq = new Request();
+    UpdateCellsRequest _updateCellsRequest = new UpdateCellsRequest();
+    _updateCellsRequest.fields = 'userEnteredValue';
+    _updateCellsRequest.range = new GridRange();
+    _updateCellsRequest.range.sheetId = 0; // sheet index
+    sheetContentsClearReq.updateCells = _updateCellsRequest;
+    batchUpdateSpreadsheetRequest.requests.add(sheetContentsClearReq);
+
+    // シートのコピー
+    var sheetCopyReq = new Request();
+
+    // シート名
+//    var sheetTitleReq = new Request();
+//    UpdateSheetPropertiesRequest updateSheetPropertiesRequest = new UpdateSheetPropertiesRequest();
+//    updateSheetPropertiesRequest.properties = new SheetProperties();
+//    updateSheetPropertiesRequest.properties.title = 'newSheetName';
+//    updateSheetPropertiesRequest.properties.index = 0;
+//    updateSheetPropertiesRequest.fields = "*";
+//    sheetTitleReq.updateSheetProperties = updateSheetPropertiesRequest;
+//    batchUpdateSpreadsheetRequest.requests.add(sheetTitleReq);
+
+    // batch update
+    sheetApi.spreadsheets.batchUpdate(batchUpdateSpreadsheetRequest, spreadsheetId);
 
     var inputValues = [
       [
-        'test input'
+        'test input2'
       ]
     ];
 
@@ -55,7 +98,6 @@ signIn() async{
     inputValueRange.values = inputValues;
 
     sheetApi.spreadsheets.values.update(inputValueRange, spreadsheetId, 'sheet1!B3', valueInputOption:'RAW');
-
 
   } catch (error) {
     print(error);
